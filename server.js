27b7427c97da106ee4a12261495306863f8d2cd0
NozomiS0129 <129555124+NozomiS0129@users.noreply.github.com>
2023-08-13 00:23:25 -0400

    fix reprinting log
    If applied, this commit will have fixed reprinting log in the terminal.

--- server.js
+++ server.js
@@ -1,36 +1,34 @@
-const http = require('http'),
-fs = require('fs'),
-url = require('url');
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
 
 http.createServer((request, response) => {
-    let addr = request.url,
-    q = url.parse(addr, true),
-    filePath = '';
-    
-    fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+    let addr = request.url;
+    let parsedUrl = url.parse(addr, true);
+    let filePath = "";
+
+    if (parsedUrl.pathname.includes("documentation")) {
+        filePath = (__dirname + "/documentation.html");
+    } else {
+        filePath = "index.html";
+    }
+
+    fs.appendFile("log.txt", `URL: ${addr}\nTimestamp: ${new Date()}\n\n`, (err) => {
         if (err) {
             console.log(err);
-        }else{
-            console.log('Added to log.');
+        }else if (addr !== "/favicon.ico"){
+            console.log("Added to log.");
         }
     });
 
-    if (q.pathname.includes('documentation')) {
-        filePath = (__dirname + '/documentation.html');
-    }else{
-        filePath = 'index.html';
-    }
-    
     fs.readFile(filePath, (err, data) => {
         if(err){
             throw err;
         }
-        
-        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.writeHead(200, { "Content-Type": "text/html" });
         response.write(data);
-        response.end();
-    });
+        response.end("");
+    });  
     
 }).listen(8080);
-
-console.log('My first Node test server is running on Port 8080.');
+console.log("My first Node test server is running on Port 8080.");

f92fdbcb0bf283a19b67c0b57f8eeaa8502b4b6d
NozomiS0129 <129555124+NozomiS0129@users.noreply.github.com>
2023-08-18 22:53:22 -0400

    add information about movies
    If applied, this commit will have added  information about movies, which is including genre, release year, and image URL, and director's birth (death)year.

--- index.js
+++ index.js
@@ -7,44 +7,106 @@ app.use(morgan("common"));
 
 let topMovies = [
     {
-        title: "The Lord Of The Rings",
-        director: "Peter Jackson"
+        Title: "2001: A Space Odyssey",
+        Director: {
+            Name: "Stanley Kubrick",
+            Birth_Year: 1928,
+            Death_Year: 1999
+        }, 
+        Genre: "Sciece Fiction",
+        Release_Year: 1968,
+        Image_Url: "https://upload.wikimedia.org/wikipedia/en/1/11/2001_A_Space_Odyssey_%281968%29.png"
     },
     {
-        title: "The Godfather",
-        director: "Francis Ford Coppola"
+        Title: "The Godfather",
+        Director: {
+            Name: "Francis Ford Coppola",
+            Birth_Year: 1939
+        }, 
+        Genre: "Gangster",
+        Release_Year: 1972,
+        Image_Url: "https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg"
     },
     {
-        title: "The Shawshank Redemption",
-        director: "Frank Darabont"
+        Title: "The Shawshank Redemption",
+        Director: {
+            Name: "Frank Darabont",
+            Birth_Year: 1959
+        }, 
+        Genre: "Drama",
+        Release_Year: 1994,
+        Image_Url: "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg"
     },
     {
         title: "Pulp Fiction",
-        director: "Quentin Tarantino"
+        Director: {
+            Name: "Quentin Tarantino",
+            Birth_Year: 1963
+        }, 
+        Genre: "Crime",
+        Release_Year: 1994,
+        Image_Url: "https://upload.wikimedia.org/wikipedia/en/3/3b/Pulp_Fiction_%281994%29_poster.jpg"
     },
     {
-        title: "Blade Runner",
-        director: "Ridley Scott"
+        Title: "Blade Runner",
+        Director: {
+            Name: "Ridley Scott",
+            Birth_Year: 1937
+        }, 
+        Genre: "Science Fiction",
+        Release_Year: 1982,
+        Image_Url: "https://upload.wikimedia.org/wikipedia/en/9/9f/Blade_Runner_%281982_poster%29.png"
     },
     {
-        title: "Fight Club",
-        director: "David Fincher"
+        Title: "Fight Club",
+        Director: {
+            Name: "David Fincher",
+            Birth_Year: 1962
+        }, 
+        Genre: "Drama",
+        Release_Year: 1999,
+        Image_Url: "https://upload.wikimedia.org/wikipedia/en/f/fc/Fight_Club_poster.jpg"
+    },
+    { 
+        Title: "Eternal Sunshine Of The Spotless Mind",
+        Director: {
+            Name: "Michel Gondry",
+            Birth_Year: 1963
+        }, 
+        Genre: "Romance",
+        Release_Year: 2004,
+        Image_Url: "https://upload.wikimedia.org/wikipedia/en/a/a4/Eternal_Sunshine_of_the_Spotless_Mind.png"
     },
     {
-        title: "Eternal Sunshine Of The Spotless Mind",
-        director: "Michel Gondry"
+        Title: "The Silence Of The Lambs",
+        Director: {
+            Name: "Jonathan Demme",
+            Birth_Year: 1944,
+            Death_Year: 2017
+        }, 
+        Genre: "Psychological Horror",
+        Release_Year: 1991,
+        Image_Url: "https://upload.wikimedia.org/wikipedia/en/8/86/The_Silence_of_the_Lambs_poster.jpg"
+    },
+    { 
+        Title: "Forrest Gump",
+        Director: {
+            Name: "Robert Zemeckis",
+            Birth_Year: 1952
+        },
+        Genre: "Drama",
+        Release_Year: 1994,
+        Image_Url: "https://upload.wikimedia.org/wikipedia/en/6/67/Forrest_Gump_poster.jpg"
     },
     {
-        title: "The Silence Of The Lambs",
-        director: "Jonathan Demme"
-    },
-    {
-        title: "Forrest Gump",
-        director: "Robert Zemeckis"
-    },
-    {
-        title: "Taxi Driver",
-        director: "Martin Scorsese"
+        Title: "Taxi Driver",
+        Director: {
+            Name: "Martin Scorsese",
+            Birth_Year: 1942
+        },
+        Genre: "Psychological Thriller",
+        Release_Year: 1976,
+        Image_Url: "https://upload.wikimedia.org/wikipedia/en/3/33/Taxi_Driver_%281976_film_poster%29.jpg"
     }
 ];
 

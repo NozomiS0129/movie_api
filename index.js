73f552a61dc95166c98d365e453f7c542ae892c2
NozomiS0129 <129555124+NozomiS0129@users.noreply.github.com>
2023-09-08 16:29:06 -0400

    replace password to hashed password
    If applied, this commit will have replaced original password to hashed password.

--- index.js
+++ index.js
@@ -42,41 +42,42 @@ app.get("/", (req, res) => {
 
 //Add new user
 app.post("/users",
-[
-    check("Username", "Username is required").isLength({min: 5}),
-    check("Username", "Username contains non alphanumeric characters - not allowed.").isAlphanumeric(),
-    check("Password", "Password is required").not().isEmpty(),
-    check("Email", "Email does not appear to be valid").isEmail()
-],
-async (req, res) => {
-    let errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(422).json({errors: errors.array() });
-    }
-    let hashPassword = Users.hashPassword(req.body.Password);
-    await Users.findOne({ Username: req.body.Username })
-    .then((user) => {
-        if (user) {
-            return res.status(400).send(req.body.Username + " already exists.");
-        } else {
-            Users.create({
-                Username: req.body.Username,
-                Password: req.body.Password,
-                Email: req.body.Email,
-                Birthday: req.body.Birthday
-            })
-            .then((user) => {res.status(201).json(user)})
-            .catch((error) => {
-                console.error(error);
-                res.status(500).send("Error: " + error);
-            })
+    [
+        check("Username", "Username is required").isLength({min: 5}),
+        check("Username", "Username contains non alphanumeric characters - not allowed.").isAlphanumeric(),
+        check("Password", "Password is required").not().isEmpty(),
+        check("Email", "Email does not appear to be valid").isEmail()
+    ],
+    async (req, res) => {
+        let errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({errors: errors.array() });
         }
-    })
-    .catch((error) => {
-        console.error(error);
-        res.status(500).send("Error" + error);
-    });
-});
+        let hashedPassword = Users.hashPassword(req.body.Password);
+        await Users.findOne({ Username: req.body.Username })
+        .then((user) => {
+            if (user) {
+                return res.status(400).send(req.body.Username + " already exists.");
+            } else {
+                Users.create({
+                    Username: req.body.Username,
+                    Password: hashedPassword,
+                    Email: req.body.Email,
+                    Birthday: req.body.Birthday
+                })
+                .then((user) => {res.status(201).json(user)})
+                .catch((error) => {
+                    console.error(error);
+                    res.status(500).send("Error: " + error);
+                })
+            }
+        })
+        .catch((error) => {
+            console.error(error);
+            res.status(500).send("Error" + error);
+        });
+    }
+);
 
 //Get all users
 app.get("/users", passport.authenticate("jwt", {session: false}), async (req, res) => {

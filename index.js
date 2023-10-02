a0cdefe4ea20e1ab7628688e32f5542787b54709
NozomiS0129 <129555124+NozomiS0129@users.noreply.github.com>
2023-10-02 12:09:50 -0400

    re-enable token based authentication
    If applied, this commit will have re-enabled token based authentication.

--- index.js
+++ index.js
@@ -183,7 +183,7 @@ app.delete("/users/:Username", passport.authenticate("jwt", {session: false}), a
 });
 
 // Get a list of ALL movies
-app.get("/movies", async (req, res) => {
+app.get("/movies", passport.authenticate("JWT", { session: false }), async (req, res) => {
     await Movies.find().then((movies) => {
         res.status(201).json(movies);
     })

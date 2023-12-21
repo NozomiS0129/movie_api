e616193c9e5c9d71a095c0b753b417864963de7b
NozomiS0129 <129555124+NozomiS0129@users.noreply.github.com>
2023-12-21 03:14:20 -0500

    fix allowed origin

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 let allowedOrigins = [
   "http://localhost:8080",
   "http://localhost:1234",
-  "https://movies-flix-app-bb16fed0a4c0.herokuapp.com/movies",
+  "https://movies-flix-app-bb16fed0a4c0.herokuapp.com",
 ];
 
 app.use(
